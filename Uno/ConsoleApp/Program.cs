d95fa4ba3e25a163f29a2a2aa2ebc1e8abb0184e
Vira Lavrova <verunia2006lav@gmail.com>
2023-10-22 21:10:46 +0300

    class for customising settings + saving into default file

--- Uno/ConsoleApp/Program.cs
+++ Uno/ConsoleApp/Program.cs
@@ -1,41 +1,68 @@
 ﻿using MenuSystem;
-var choiceOfSettingToCustomise = new Menu(title:"Choose what setting you'd like to customize", new List<MenuItem>()
+var directionPrompt = new Prompt<string>(
+    "Enter playing direction (clockwise or counterclockwise): ", 
+    new List<string> { "clockwise", "counterclockwise" }
+);
+var gameSettingsToCustomize = new Menu(title:"Choose what category of settings you'd like to customize", new List<MenuItem>()
 {
     new MenuItem()
     {
         Shortcut = "1",
-        MenuLabel = "Setting 1",
+        MenuLabel = "Number of decks",
     },
     new MenuItem()
     {
         Shortcut = "2",
-        MenuLabel = "Setting 2"
+        MenuLabel = "Winning score",
     },
     new MenuItem()
     {
-        Shortcut = "f",
-        MenuLabel = "Finish customisation"
+        Shortcut = "3",
+        MenuLabel = "Playing direction",
+        MethodToRun = () => 
+        {
+            string userChoice = directionPrompt.GetUserInput();
+            directionPrompt.UpdateSetting("gameSettings:PlayDirection", userChoice, "/home/viralavrova/cardgameuno/Uno/Resources/default_settings.json");
+
+            Console.WriteLine($"Playing direction updated to: {userChoice}");
+            return "back";  
+        }    }
+}, EMenuLevel.Other);
+
+var kindofSettingsToCustomize = new Menu(title:"Choose what category of settings you'd like to customize", new List<MenuItem>()
+{
+    new MenuItem()
+    {
+        Shortcut = "1",
+        MenuLabel = "Card settings"
+    },
+    new MenuItem()
+    {
+        Shortcut = "2",
+        MenuLabel = "Game settings",
+        MethodToRun = gameSettingsToCustomize.Run
     }
 }, EMenuLevel.Other);
+
 var whatToDoWithSettings = new Menu(title:"Choose what you want to do with these settings", new List<MenuItem>()
 {
     new MenuItem()
     {
         Shortcut = "1",
         MenuLabel = "Use only for this game (without saving)",
-        MethodToRun = choiceOfSettingToCustomise.Run,
+        MethodToRun = kindofSettingsToCustomize.Run,
     },
     new MenuItem()
     {
         Shortcut = "2",
         MenuLabel = "Use for this game and save into file",
-        MethodToRun = choiceOfSettingToCustomise.Run,
+        MethodToRun = kindofSettingsToCustomize.Run,
     },
     new MenuItem()
     {
         Shortcut = "3",
         MenuLabel = "Use for this game and set as default for further games",
-        MethodToRun = choiceOfSettingToCustomise.Run,
+        MethodToRun = kindofSettingsToCustomize.Run,
     }
 }, EMenuLevel.Other);
 var settingsChoice = new Menu(title:"Choose what settings you want to continue with", new List<MenuItem>()
---
+++ Uno/MenuSystem/Prompt.cs
@@ -0,0 +1,86 @@
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace MenuSystem;
+    public class Prompt<T>
+    {
+        public string Question { get; }
+        public List<T> AllowedValues { get; }
+
+        public Prompt(string question, List<T> allowedValues)
+        {
+            Question = question;
+            AllowedValues = allowedValues;
+        }
+
+        public T GetUserInput()
+        {
+            T? userInput = default(T); 
+            while (true)
+            {
+                Console.WriteLine(Question);
+                try
+                {
+                    userInput = (T)Convert.ChangeType(Console.ReadLine()!, typeof(T));
+
+                    if (AllowedValues.Contains(userInput))
+                    {
+                        break; // exit the loop as we've got a valid input.
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Invalid selection. Please choose from the following: {string.Join(", ", AllowedValues)}");
+                    }
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Invalid format. Please try again.");
+                }
+                catch (InvalidCastException)
+                {
+                    Console.WriteLine("This type of input is not supported.");
+                }
+            }
+
+            return userInput;
+        }
+
+        public void UpdateSetting(string jsonKey, T value, string filePath)
+        {
+            string jsonData = File.ReadAllText(filePath);
+            JObject jsonObject = JObject.Parse(jsonData);
+            string[] keyParts = jsonKey.Split(':');
+
+            JToken currentToken = jsonObject;
+            for (int i = 0; i < keyParts.Length; i++)
+            {
+                if (currentToken == null)
+                {
+                    throw new NullReferenceException($"Missing element at {string.Join(':', keyParts, 0, i)}");
+                }
+
+                JToken? nextToken = currentToken[keyParts[i]];
+
+                if (nextToken == null)
+                {
+                    throw new KeyNotFoundException($"Key not found: {string.Join(':', keyParts, 0, i + 1)}");
+                }
+                else if (nextToken is JObject)
+                {
+                    currentToken = nextToken;
+                }
+                else if (i == keyParts.Length - 1)
+                {
+                    JToken valueToReplace = JToken.FromObject(value);
+                    currentToken[keyParts[i]]?.Replace(valueToReplace);
+                }
+                else
+                {
+                    currentToken = nextToken;
+                }
+            }
+            jsonData = jsonObject.ToString();
+            File.WriteAllText(filePath, jsonData);
+            Console.WriteLine("Settings have been successfully updated!");
+        }
+    }
